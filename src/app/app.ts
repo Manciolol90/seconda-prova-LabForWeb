db7f374603d4d4e327b5241200484d0bddcbbb96
Daniele Mancini <daniele.mancio.90@gmail.com>
2025-12-01 14:50:26 +0100

    creato primo scheletro sito

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Footer } from './components/footer/footer';
+import { Slider } from './components/slider/slider';
+import { Header } from './components/header/header';
+import { HeroBanner } from './components/hero-banner/hero-banner';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Footer, Header, Slider, HeroBanner],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('seconda-prova');
